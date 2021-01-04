b179c2d74aa64f7953eecd20299f2dfdc397d96a
hammer <lava_hammer@163.com>
2021-01-04 09:00:27 +0800

    setup debug

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rs2ts
 a transpiler turning rust into typescript
+
+# it's under construction, not a working project for now.
+
+## How to debug typescript with VSCode
+see https://code.visualstudio.com/docs/typescript/typescript-debugging
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,12 @@ import fs from 'fs';
 import { tokenize, untokenize } from './tokenize';
 
 function main() {
-  // const src = fs.readFileSync('test.rs', {
-  const src = fs.readFileSync('message_meta_db.rs', {
+  const src = fs.readFileSync('test/test.rs', {
     encoding: 'utf-8'
   });
   const tokens = tokenize(src);
   const rsrc = untokenize(tokens);
-  fs.writeFileSync('output.rs', rsrc, {
+  fs.writeFileSync('test/out.rs', rsrc, {
     encoding: 'utf-8',
   });
 }
